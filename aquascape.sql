2d999d0ddb4315fb8ee3f3ef1ad591efdd6a1477
mohberliannusantara <liannusantara1158@gmail.com>
2019-12-19 14:29:30 +0700

    update database

--- aquascape.sql
+++ aquascape.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.6.6deb5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 18, 2019 at 10:21 PM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.2
+-- Host: localhost:3306
+-- Generation Time: Dec 19, 2019 at 02:28 PM
+-- Server version: 5.7.21-1
+-- PHP Version: 7.0.29-1+b1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -91,7 +89,7 @@ CREATE TABLE `detail_transaksi` (
 
 CREATE TABLE `kategori` (
   `id_kategori` int(11) NOT NULL,
-  `nama` varchar(45) DEFAULT NULL,
+  `nama_kategori` varchar(45) DEFAULT NULL,
   `gambar` varchar(255) NOT NULL,
   `deskripsi` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -100,14 +98,14 @@ CREATE TABLE `kategori` (
 -- Dumping data for table `kategori`
 --
 
-INSERT INTO `kategori` (`id_kategori`, `nama`, `gambar`, `deskripsi`) VALUES
-(1, 'Aquatic Plants', '', ''),
-(2, 'Co2', '', ''),
-(3, 'Nutrients', '', ''),
-(4, 'Shrimps', '', ''),
-(5, 'Lighting', '', ''),
-(6, 'Aquarium Tank', '', ''),
-(7, 'Filtrations', '', '');
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`, `gambar`, `deskripsi`) VALUES
+(1, 'Co2', '2129c0ce918c828e836f0c73c6dff68c.jpg', ''),
+(2, 'Aquatic Plants', '3876fc6e5c0f9f2cd6f0a4a39d7601b8.jpg', ''),
+(3, 'Filtrations', '96770bedfa962abe8944a53041f708a6.jpg', ''),
+(4, 'Shrimps', 'avoid-overfeeding-your-fish.jpg', ''),
+(5, 'Lighting', '1dc53e4100068385569682dc6871f01f.jpg', ''),
+(6, 'Nutrients', 'Marimo-Moss-Balls.jpg', ''),
+(7, 'Aquarium Tank', 'bbe5557cc51077f672ecfbf42ff42949.jpg', '');
 
 -- --------------------------------------------------------
 
@@ -119,7 +117,6 @@ CREATE TABLE `keranjang` (
   `id_keranjang` int(11) NOT NULL,
   `id_produk` int(11) NOT NULL,
   `jumlah` int(11) NOT NULL,
-  `id_pengguna` int(11) NOT NULL,
   `subtotal` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -127,9 +124,9 @@ CREATE TABLE `keranjang` (
 -- Dumping data for table `keranjang`
 --
 
-INSERT INTO `keranjang` (`id_keranjang`, `id_produk`, `jumlah`, `id_pengguna`, `subtotal`) VALUES
-(1, 4, 2, 2, 4000000),
-(4, 1, 3, 2, 15000);
+INSERT INTO `keranjang` (`id_keranjang`, `id_produk`, `jumlah`, `subtotal`) VALUES
+(1, 4, 2, 4000000),
+(4, 1, 3, 15000);
 
 -- --------------------------------------------------------
 
@@ -159,7 +156,7 @@ INSERT INTO `level` (`id_level`, `nama`) VALUES
 
 CREATE TABLE `merk` (
   `id_merk` int(11) NOT NULL,
-  `nama` varchar(128) NOT NULL,
+  `nama_merk` varchar(128) NOT NULL,
   `telephone` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -167,7 +164,7 @@ CREATE TABLE `merk` (
 -- Dumping data for table `merk`
 --
 
-INSERT INTO `merk` (`id_merk`, `nama`, `telephone`) VALUES
+INSERT INTO `merk` (`id_merk`, `nama_merk`, `telephone`) VALUES
 (1, 'kodak', '086695232'),
 (2, 'berlian', '083636343');
 
@@ -217,47 +214,15 @@ INSERT INTO `pengaturan` (`id_pengaturan`, `ongkir`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pengguna`
---
-
-CREATE TABLE `pengguna` (
-  `id_pengguna` int(11) NOT NULL,
-  `nama` varchar(45) DEFAULT NULL,
-  `telp` varchar(15) DEFAULT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  `password` text,
-  `latitude` float DEFAULT NULL,
-  `longitude` float DEFAULT NULL,
-  `foto` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `pengguna`
---
-
-INSERT INTO `pengguna` (`id_pengguna`, `nama`, `telp`, `email`, `password`, `latitude`, `longitude`, `foto`) VALUES
-(2, 'm. nindra zaka', '085331247098', 'mnindrazaka@gmail.com', 'a7f561e744962f5fed246a51067bbd72', -7.95873, 112.638, '8c57cc1c4ad167d01bf26f4581248af8.png'),
-(3, 'Moh. Berlian Nusantara', '085463527362', 'berlian@gmail.com', '81373d0406b0661e30de35cfbeb57413', -8.20468, 114.38, ''),
-(4, 'eri', '085331247098', 'eri@gmail.com', 'eri', 0.56789, 0.45678, ''),
-(5, 'raka ardianata baida', '085331247098', 'rakaardianata@gmail.com', '38a958280e3602e248b8f341110e6fb3', NULL, NULL, '95583238b0546ed34cbd9052c1b2243a.png'),
-(6, 'ananta', NULL, 'ananta@gmail.com', 'e918c9572cd5f2c9df880ccb8736eecd', NULL, NULL, NULL),
-(7, 'ardhana', '085331247098', 'ardhana@gmail.com', '9d906487acc499108bb12b2889706ec7', -7.95541, 112.632, '6751c22d4ac1acf68cd2087748ce3d01.png'),
-(8, 'aditya', '085331247098', 'aditya@gmail.com', '057829fa5a65fc1ace408f490be486ac', -7.96277, 112.636, NULL),
-(9, 'fannyervan', NULL, 'fannyervan@gmail.com', 'ff7af2c7ac9fd099d81d52dacbe25811', -7.95873, 112.638, NULL),
-(10, 'wowo', NULL, 'wowo@gmail.com', '167745795e206a7248286b998a7d625c', -7.95873, 112.638, NULL);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `produk`
 --
 
 CREATE TABLE `produk` (
   `id_produk` int(11) NOT NULL,
-  `nama` varchar(45) DEFAULT NULL,
+  `nama_produk` varchar(45) DEFAULT NULL,
   `deskripsi` varchar(256) DEFAULT NULL,
   `id_kategori` int(11) NOT NULL,
-  `foto` varchar(45) DEFAULT NULL,
+  `gambar_produk` varchar(45) DEFAULT NULL,
   `harga` varchar(128) DEFAULT NULL,
   `stock` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -266,19 +231,21 @@ CREATE TABLE `produk` (
 -- Dumping data for table `produk`
 --
 
-INSERT INTO `produk` (`id_produk`, `nama`, `deskripsi`, `id_kategori`, `foto`, `harga`, `stock`) VALUES
-(1, 'Black Lava', '', 1, 'ikan mujair.png', '5000', 10),
-(2, 'Filter Guard', '', 2, 'ikan gurami.png', '5000', 5),
-(4, 'Glass In Flow', '', 2, '9ec5eb77459be22a5e59597382bc4e91.jpg', '2000000', 10),
-(5, 'Aqua Soil Amazonia', '', 1, '764b6627b6fffda92cafa67631ecd35c.jpg', '100000', 20);
+INSERT INTO `produk` (`id_produk`, `nama_produk`, `deskripsi`, `id_kategori`, `gambar_produk`, `harga`, `stock`) VALUES
+(1, 'Black Lava', '', 1, 'black-lava_300px.jpg', '5000', 10),
+(2, 'Filter Guard', '', 2, 'aquasabi-filter-guard-fine-mesh_300px.jpg', '5000', 5),
+(4, 'Glass Inflow', '', 2, 'aqua-rebell-glass-inflow_300px.jpg', '200000', 10),
+(5, 'Aqua Soil Amazonia', '', 1, 'ada-aqua-soil-amazonia_300px.jpg', '100000', 20);
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `transaksi`
 --
--- Error reading structure for table aquascape.transaksi: #1932 - Table 'aquascape.transaksi' doesn't exist in engine
--- Error reading data for table aquascape.transaksi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `aquascape`.`transaksi`' at line 1
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -344,17 +311,17 @@ ALTER TABLE `pengaturan`
   ADD PRIMARY KEY (`id_pengaturan`);
 
 --
--- Indexes for table `pengguna`
---
-ALTER TABLE `pengguna`
-  ADD PRIMARY KEY (`id_pengguna`);
-
---
 -- Indexes for table `produk`
 --
 ALTER TABLE `produk`
   ADD PRIMARY KEY (`id_produk`) USING BTREE,
   ADD KEY `fk_kategori_id` (`id_kategori`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -365,61 +332,51 @@ ALTER TABLE `produk`
 --
 ALTER TABLE `admin`
   MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
 --
 -- AUTO_INCREMENT for table `akses`
 --
 ALTER TABLE `akses`
   MODIFY `id_akses` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
-
 --
 -- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
   MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
 --
 -- AUTO_INCREMENT for table `keranjang`
 --
 ALTER TABLE `keranjang`
   MODIFY `id_keranjang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
 --
 -- AUTO_INCREMENT for table `level`
 --
 ALTER TABLE `level`
   MODIFY `id_level` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
 --
 -- AUTO_INCREMENT for table `merk`
 --
 ALTER TABLE `merk`
   MODIFY `id_merk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
 --
 -- AUTO_INCREMENT for table `modul`
 --
 ALTER TABLE `modul`
   MODIFY `id_modul` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
 --
 -- AUTO_INCREMENT for table `pengaturan`
 --
 ALTER TABLE `pengaturan`
   MODIFY `id_pengaturan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `pengguna`
---
-ALTER TABLE `pengguna`
-  MODIFY `id_pengguna` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
 --
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
   MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
@@ -438,18 +395,10 @@ ALTER TABLE `akses`
   ADD CONSTRAINT `akses_ibfk_2` FOREIGN KEY (`id_level`) REFERENCES `level` (`id_level`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `detail_transaksi`
---
-ALTER TABLE `detail_transaksi`
-  ADD CONSTRAINT `fk_produk_has_transaksi_produk1` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`) ON DELETE SET NULL ON UPDATE SET NULL,
-  ADD CONSTRAINT `fk_produk_has_transaksi_transaksi1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
 -- Constraints for table `produk`
 --
 ALTER TABLE `produk`
   ADD CONSTRAINT `fk_kategori_id` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`);
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
